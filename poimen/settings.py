d09bee92b8d1473b8bad24af211005453bcd3a45
samuelbahiru93@gmail.com <samuelbahiru93@gmail.com>
2023-07-02 17:20:36 +0300

    index html readmore button configured

--- poimen/settings.py
+++ poimen/settings.py
@@ -150,12 +150,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ...
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 25  # Use the port number of your local SMTP server
-EMAIL_HOST_USER = ''  
-EMAIL_HOST_PASSWORD = ''  
-EMAIL_USE_TLS = False  # Set it to True if your local SMTP server requires TLS
-DEFAULT_FROM_EMAIL = 'samuelbahiru93@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587  
+EMAIL_HOST_USER = 'samuelbahiru93@gmail.com '  
+EMAIL_HOST_PASSWORD = '1221'  
+EMAIL_USE_TLS = True 
+# DEFAULT_FROM_EMAIL = 'samuelbahiru93@gmail.com'
 
 # ...
 
